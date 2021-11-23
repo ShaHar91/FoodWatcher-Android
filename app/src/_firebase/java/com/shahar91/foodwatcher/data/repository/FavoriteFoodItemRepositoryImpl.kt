fa62fa17499dc8aa45c0f87c6aa63c7ef796c03a
ShaHar91 <bollachristiano@gmail.com>
2021-11-23 10:21:50 +0100

    FoodItems can be favorited when needed

--- app/src/_firebase/java/com/shahar91/foodwatcher/data/repository/FavoriteFoodItemRepositoryImpl.kt
+++ app/src/_firebase/java/com/shahar91/foodwatcher/data/repository/FavoriteFoodItemRepositoryImpl.kt
@@ -1,8 +1,20 @@
 package com.shahar91.foodwatcher.data.repository
 
+import com.google.firebase.firestore.FirebaseFirestore
+
 
 class FavoriteFoodItemRepositoryImpl : FavoriteFoodItemRepository {
-    override suspend fun favoriteFoodItem(foodItemId: String): Long = -1
 
-    override suspend fun unFavoriteFoodItem(foodItemId: String) {    }
+    companion object {
+        private const val COLLECTION_FOOD_ITEM = "foodItems"
+    }
+
+    override suspend fun favoriteFoodItem(foodItemId: String): Long {
+        FirebaseFirestore.getInstance().collection(COLLECTION_FOOD_ITEM).document(foodItemId).update("favorite", true)
+        return -1L
+    }
+
+    override suspend fun unFavoriteFoodItem(foodItemId: String) {
+        FirebaseFirestore.getInstance().collection(COLLECTION_FOOD_ITEM).document(foodItemId).update("favorite", false)
+    }
 }
