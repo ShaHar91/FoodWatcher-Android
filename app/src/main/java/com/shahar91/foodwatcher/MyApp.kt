4c075de7e57dd0cecd926ad41e140729533821ad
ShaHar91 <bollachristiano@gmail.com>
2021-11-29 22:16:44 +0100

    The app only supports English and Dutch translations, when something else is used for the device it defaults to English. This makes sure the Calendar headings are always correct as well!

--- app/src/main/java/com/shahar91/foodwatcher/MyApp.kt
+++ app/src/main/java/com/shahar91/foodwatcher/MyApp.kt
@@ -1,14 +1,41 @@
 package com.shahar91.foodwatcher
 
 import android.app.Application
+import android.content.Context
+import android.content.res.Configuration
+import android.content.res.Resources
 import be.appwise.core.core.CoreApp
+import com.akexorcist.localizationactivity.core.LanguageSetting
+import com.akexorcist.localizationactivity.core.LocalizationApplicationDelegate
 import com.shahar91.foodwatcher.di.KoinInitializer
+import java.util.*
 
 class MyApp : Application() {
-    companion object {
 
+    private val localizationDelegate = LocalizationApplicationDelegate()
+    private lateinit var deviceLanguage: Locale
+
+    companion object {
         lateinit var instance: MyApp
             private set
+
+        /**
+         * Supported languages for Orange: English, French, German & Dutch
+         * English is default
+         *
+         * @param localeLanguage Locale
+         * @return one of the supported languages
+         */
+        fun getDefaultLocaleForProject(localeLanguage: Locale): Locale {
+            return if (localeLanguage.language == Locale("nl", "").language
+//                || localeLanguage.language == Locale("fr", "").language ||
+//                localeLanguage.language == Locale("de", "").language
+            ) {
+                localeLanguage
+            } else {
+                Locale.ENGLISH
+            }
+        }
     }
 
     override fun onCreate() {
@@ -17,6 +44,11 @@ class MyApp : Application() {
 
         initKoin()
         initCore()
+
+        localizationDelegate.setDefaultLanguage(this, getDefaultLocaleForProject(deviceLanguage))
+        // after the default language is set, the LanguageSetting needs to be adjusted in order to take effect through the complete app...
+        // something strange, but this makes it work
+        LanguageSetting.setLanguage(this, getDefaultLocaleForProject(deviceLanguage))
     }
 
     private fun initKoin() {
@@ -28,5 +60,24 @@ class MyApp : Application() {
             .initializeErrorActivity(BuildConfig.DEBUG)
             .initializeLogger(getString(R.string.app_name), BuildConfig.DEBUG)
             .build()
+    }
+
+    override fun attachBaseContext(base: Context) {
+        deviceLanguage = Locale.getDefault()
+        localizationDelegate.setDefaultLanguage(base, deviceLanguage)
+        super.attachBaseContext(localizationDelegate.attachBaseContext(base))
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        localizationDelegate.onConfigurationChanged(this)
+    }
+
+    override fun getApplicationContext(): Context {
+        return localizationDelegate.getApplicationContext(super.getApplicationContext())
+    }
+
+    override fun getResources(): Resources {
+        return localizationDelegate.getResources(baseContext)
     }
 }
--- app/src/main/java/com/shahar91/foodwatcher/ui/MainActivity.kt
+++ app/src/main/java/com/shahar91/foodwatcher/ui/MainActivity.kt
@@ -1,12 +1,55 @@
 package com.shahar91.foodwatcher.ui
 
+import android.content.Context
+import android.content.res.Resources
 import android.os.Bundle
 import be.appwise.core.ui.base.BaseActivity
+import com.akexorcist.localizationactivity.core.LocalizationActivityDelegate
+import com.akexorcist.localizationactivity.core.OnLocaleChangedListener
 import com.shahar91.foodwatcher.R
+import java.util.*
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity(), OnLocaleChangedListener {
+
+    private val localizationDelegate = LocalizationActivityDelegate(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        localizationDelegate.addOnLocaleChangedListener(this)
+        localizationDelegate.onCreate()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    public override fun onResume() {
+        super.onResume()
+        localizationDelegate.onResume(this)
+    }
+
+    override fun attachBaseContext(newBase: Context) {
+        applyOverrideConfiguration(localizationDelegate.updateConfigurationLocale(newBase))
+        super.attachBaseContext(newBase)
+    }
+
+    override fun getApplicationContext(): Context {
+        return localizationDelegate.getApplicationContext(super.getApplicationContext())
+    }
+
+    override fun getResources(): Resources {
+        return localizationDelegate.getResources(super.getResources())
+    }
+
+    fun setLanguage(language: String?) {
+        localizationDelegate.setLanguage(this, language!!)
+    }
+
+    fun setLanguage(locale: Locale?) {
+        localizationDelegate.setLanguage(this, locale!!)
+    }
+
+    val currentLanguage: Locale
+        get() = localizationDelegate.getLanguage(this)
+
+    override fun onAfterLocaleChanged() {}
+
+    override fun onBeforeLocaleChanged() {}
 }
