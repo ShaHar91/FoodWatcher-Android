9e0b526ce57f2d69c7e688617bd1a43cc2cd7070
ShaHar91 <bollachristiano@gmail.com>
2021-12-18 22:12:59 +0100

    Rearranged the code a little bit so it'll be easier to traverse/navigate

--- CurveTracker/src/main/java/be/shahar91/curvetracker/CurveTracker.kt
+++ CurveTracker/src/main/java/be/shahar91/curvetracker/CurveTracker.kt
@@ -24,31 +24,106 @@ class CurveTracker @JvmOverloads constructor(
         private const val STROKE_WIDTH = 6f
     }
 
-    private val arcRect = RectF()
-    private val rect = Rect()
-    private var diameter = 0f
-    private var startAngle = 180f
-    private var angle = 180f
-
-    private var minValue: Float = 0f
-    var ctMaxValue: Float = DEFAULT_MAX_VALUE
-        set(value) {
-            field = value
-            drawMax()
-            drawProgress()
-        }
+    // <editor-fold desc="attributes">
     var ctProgressValue: Float = 0f
         set(value) {
             field = value
-            drawProgress()
+            animateProgress()
         }
 
+    var ctMaxValue: Float = DEFAULT_MAX_VALUE
+        set(value) {
+            field = value
+            animateMax()
+            animateProgress()
+        }
+
+    private val minValueText: String
+        get() = "0"
+    private val maxValueText: String
+        get() = if (ctProgressValue < ctMaxValue) "$ctMaxValue" else "$ctProgressValue"
+    // </editor-fold>
+    
+    private val arcRect = RectF()
+    private val rect = Rect()
+    private var diameter = 0f
+    private val radius: Float
+        get() = diameter / 2
+
+    /**
+     * The starting angle to draw the arc at. The 0° angle for Android is at the right (instead at the top)
+     */
+    private var startAngle = 180f
+
+    /**
+     * The sweepAngle to the draw the arc to. This will technically be added to the [startAngle] to draw the full arc.
+     * Using a positive number will draw it clockwise, using a negative number will be counterclockwise
+     */
+    private var angle = 180f
+
+    // <editor-fold desc="Animations">
     private var animatorProgress: ValueAnimator? = null
     private var animationProgressValue = 0f
     private var animatorOverflow: ValueAnimator? = null
     private var animationOverflowValue = 0f
     private var animationMaxValue = 0f
 
+    /**
+     * Will only be called once when this view is first created.
+     * Using [animateProgress] right after this, will give a nice starting animation
+     */
+    private fun animateMax() {
+        ValueAnimator.ofFloat(0f, 1f).apply {
+            interpolator = DecelerateInterpolator()
+            duration = ANIMATION_DURATION
+            addUpdateListener {
+                animationMaxValue = it.animatedValue as Float
+                invalidate()
+            }
+            start()
+        }
+    }
+
+    /**
+     * Will be called anytime the [ctProgressValue] changes.
+     * Any previous running animation would need to be cancelled before starting a new one.
+     */
+    private fun animateProgress() {
+        animationOverflowValue = 0f
+        animatorProgress?.cancel()
+        animatorProgress = ValueAnimator.ofFloat(0f, 1f).apply {
+            interpolator = DecelerateInterpolator()
+            duration = if (ctProgressValue > ctMaxValue) 350 else ANIMATION_DURATION
+            addUpdateListener {
+                animationProgressValue = it.animatedValue as Float
+                invalidate()
+                if (it.animatedValue == 1F && ctProgressValue > ctMaxValue) {
+                    animateOverflow()
+                }
+            }
+            start()
+        }
+    }
+
+    /**
+     * Will be called when the [animateProgress] animation has ended AND the [ctProgressValue] is higher than the [ctMaxValue].
+     * Any previous running animation would need to be cancelled before starting a new one.
+     */
+    private fun animateOverflow() {
+        animatorOverflow?.cancel()
+        animatorOverflow = ValueAnimator.ofFloat(0f, 1f).apply {
+            interpolator = DecelerateInterpolator()
+            duration = ANIMATION_DURATION
+            addUpdateListener {
+                animationOverflowValue = it.animatedValue as Float
+                invalidate()
+            }
+            start()
+        }
+    }
+    // </editor-fold>
+
+    // <editor-fold desc="Paints">
     private var backgroundPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         color = ContextCompat.getColor(context, R.color.on_floating_athens_gray)
         style = Paint.Style.STROKE
@@ -73,13 +148,6 @@ class CurveTracker @JvmOverloads constructor(
         isAntiAlias = true
     }
 
-    private val minTextPaint = TextPaint().apply {
-        isAntiAlias = true
-        color = ContextCompat.getColor(context, R.color.dodger_blue)
-        textSize = Dimension.dp(12f, context).asPx()
-        textAlign = Paint.Align.LEFT
-    }
-
     // TODO textAppearance on paint... https://stackoverflow.com/a/64423035/2263408
     private val maxTextPaint = TextPaint().apply {
         isAntiAlias = true
@@ -87,6 +155,14 @@ class CurveTracker @JvmOverloads constructor(
         textSize = Dimension.dp(12f, context).asPx()
         textAlign = Paint.Align.RIGHT
     }
+
+    private val minTextPaint = TextPaint().apply {
+        isAntiAlias = true
+        color = ContextCompat.getColor(context, R.color.dodger_blue)
+        textSize = Dimension.dp(12f, context).asPx()
+        textAlign = Paint.Align.LEFT
+    }
+    // </editor-fold>
 
     init {
         attributeSet?.let {
@@ -97,8 +173,8 @@ class CurveTracker @JvmOverloads constructor(
             a.recycle()
         }
 
-        drawMax()
-        drawProgress()
+        animateProgress()
+        animateMax()
 
         if (isInEditMode) {
             animationMaxValue = 1f
@@ -107,54 +183,16 @@ class CurveTracker @JvmOverloads constructor(
         }
     }
 
-    private fun drawMax() {
-        ValueAnimator.ofFloat(0f, 1f).apply {
-            interpolator = DecelerateInterpolator()
-            duration = ANIMATION_DURATION
-            addUpdateListener {
-                animationMaxValue = it.animatedValue as Float
-                invalidate()
-            }
-            start()
-        }
-    }
-
-    private fun drawProgress() {
-        animationOverflowValue = 0f
-        animatorProgress?.cancel()
-        animatorProgress = ValueAnimator.ofFloat(0f, 1f).apply {
-            interpolator = DecelerateInterpolator()
-            duration = ANIMATION_DURATION
-            addUpdateListener {
-                animationProgressValue = it.animatedValue as Float
-                invalidate()
-                if (it.animatedValue == 1F && ctProgressValue > ctMaxValue) {
-                    drawOverflow()
-                }
-            }
-            start()
-        }
-    }
-
-    private fun drawOverflow() {
-        animatorOverflow?.cancel()
-        animatorOverflow = ValueAnimator.ofFloat(0f, 1f).apply {
-            interpolator = DecelerateInterpolator()
-            duration = ANIMATION_DURATION
-            addUpdateListener {
-                animationOverflowValue = it.animatedValue as Float
-                invalidate()
-            }
-            start()
-        }
-    }
-
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
         updateRectForArc()
+
+        // Draw background
         canvas.drawArc(arcRect, startAngle, angle * animationMaxValue, false, backgroundPaint)
+        // Draw progress
         canvas.drawArc(arcRect, startAngle, angle * (min(ctMaxValue, ctProgressValue) / ctMaxValue) * animationProgressValue, false, progressPaint)
+        // Draw overflow
         if (ctProgressValue > ctMaxValue) {
             canvas.drawArc(arcRect, 0f, -(angle * (ctProgressValue - ctMaxValue) / ctProgressValue * animationOverflowValue), false, overflowPaint)
         }
@@ -164,19 +202,19 @@ class CurveTracker @JvmOverloads constructor(
     }
 
     private fun drawMaxText(canvas: Canvas) {
-        maxTextPaint.getTextBounds(getMaxValueText(), 0, getMaxValueText().length, rect)
+        maxTextPaint.getTextBounds(maxValueText, 0, maxValueText.length, rect)
 
         val (_, ty) = getCoordinatesForText(6f)
 
-        canvas.drawText(getMaxValueText(), arcRect.right + backgroundPaint.strokeWidth - paddingRight, ty, maxTextPaint)
+        canvas.drawText(maxValueText, arcRect.right + backgroundPaint.strokeWidth - paddingRight, ty, maxTextPaint)
     }
 
     private fun drawMinText(canvas: Canvas) {
-        minTextPaint.getTextBounds(getMinValueText(), 0, getMinValueText().length, rect)
+        minTextPaint.getTextBounds(minValueText, 0, minValueText.length, rect)
 
         val (_, ty) = getCoordinatesForText(18f)
 
-        canvas.drawText(getMinValueText(), arcRect.left - backgroundPaint.strokeWidth + paddingLeft, ty, minTextPaint)
+        canvas.drawText(minValueText, arcRect.left - backgroundPaint.strokeWidth + paddingLeft, ty, minTextPaint)
     }
 
     private fun getCoordinatesForText(angle: Float): Pair<Float, Float> {
@@ -186,19 +224,18 @@ class CurveTracker @JvmOverloads constructor(
         val angleFrom12 = angle / 24.0 * 2.0 * Math.PI
         val correctAngle = Math.PI / 2.0 - angleFrom12
 
-        val tx = (circleCenterX + cos(correctAngle) * (diameter / 2))
-        val ty = (circleCenterY - sin(correctAngle) * (diameter / 2)) + (rect.height() * 2)
+        val tx = (circleCenterX + cos(correctAngle) * radius)
+        val ty = (circleCenterY - sin(correctAngle) * radius) + (rect.height() * 2)
 
 //        val ty = ((arcRect.bottom - tyTemp) / 2) + tyTemp
 
         return tx.toFloat() to ty.toFloat()
     }
 
-    private fun getMinValueText() = "$minValue"
-    private fun getMaxValueText() = if (ctProgressValue < ctMaxValue) {
-        "$ctMaxValue"
-    } else {
-        "$ctProgressValue"
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
+
     }
 
     override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
@@ -211,8 +248,8 @@ class CurveTracker @JvmOverloads constructor(
     private fun updateRectForArc() {
         val strokeWidth = backgroundPaint.strokeWidth
 
-        val dirtyLeft = (width / 2) - (diameter / 2)
-        val dirtyTop = (height / 2) - (diameter / 2)
+        val dirtyLeft = (width / 2) - radius
+        val dirtyTop = (height / 2) - radius
 
         val left = dirtyLeft + strokeWidth + paddingLeft
         val top = dirtyTop + strokeWidth + paddingTop
